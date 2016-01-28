f4d3c744c64fd15424d5950d17de5d6235dd5d28
Tore <tore@norbits.no>
2016-01-28 21:25:46 +0100

    Optionally taking device address as command line argument

--- igrill.py
+++ igrill.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 
 import time
-from peripherals import find_peripheral, UUIDS
+import sys
+from peripherals import find_peripheral, IGrillMiniPeripheral, UUIDS
 
 
 if __name__ == "__main__":
     while True:
         try:
-
-            #find igrill device
-            print "Scanning for devices..."
-            peripheral = find_peripheral()
-            print "Found ", peripheral.addr
+            if len(sys.argv) > 1:
+                #if we have a command line argument, treat it as device address
+                peripheral = IGrillMiniPeripheral(sys.argv[1])
+            else:
+                #otherwise scan for ble device
+                print "Scanning for devices..."
+                peripheral = find_peripheral()
+                print "Found ", peripheral.addr
 
             #find characteristics for battery and temperature
             battery_char = peripheral.characteristic(UUIDS.BATTERY_LEVEL)
